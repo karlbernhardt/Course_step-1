ccfd9b6e3d93407597e013103230e235dbeb852d
Karl Bernhardt <karlbernhardt50@gmail.com>
2021-08-19 21:16:03 +0200

    Initial Commit

---
+++ Course_step 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Course_step 1
+//
+//  Created by Karl Bernhardt on 8/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
